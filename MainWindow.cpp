166bcb7a7da932fab645b76b97740223852b544d
Mahdi <144210136+Mahediali49@users.noreply.github.com>
2026-07-25 18:25:09 +0530

    feat: integrate Daheng Galaxy SDK for dual camera acquisition and image processing support

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,14 +1,116 @@
 #include "MainWindow.h"
-#include "./ui_MainWindow.h"
+#include "ui_MainWindow.h"
+
+#include <QDebug>
+#include <QTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    if(initializeGalaxy())
+    {
+        detectCameras();
+    }
+}
+
+bool MainWindow::initializeGalaxy()
+{
+    try
+    {
+        IGXFactory::GetInstance().Init();
+
+        addLog("Galaxy SDK Initialized");
+
+        return true;
+    }
+    catch(...)
+    {
+        addLog("Galaxy SDK Initialization Failed");
+
+        return false;
+    }
+}
+
+void MainWindow::detectCameras()
+{
+    gxdeviceinfo_vector deviceInfoList;
+
+    IGXFactory::GetInstance().UpdateDeviceList(1000, deviceInfoList);
+
+    int count = static_cast<int>(deviceInfoList.size());
+
+    addLog(QString("Detected %1 Camera(s)").arg(count));
+
+    if (count > 0)
+    {
+        m_camera1Connected = true;
+        openCamera(0);
+        addLog("Camera 1 Connected");
+    }
+    else
+    {
+        addLog("Camera 1 Not Connected");
+    }
+
+    if (count > 1)
+    {
+        m_camera2Connected = true;
+        openCamera(1);
+        addLog("Camera 2 Connected");
+    }
+    else
+    {
+        addLog("Camera 2 Not Connected");
+    }
+}
+
+void MainWindow::openCamera(int index)
+{
+    try
+    {
+        // Open device
+        // Open stream
+        // Start grabbing
+    }
+    catch(...)
+    {
+        addLog(QString("Failed to Open Camera %1").arg(index + 1));
+    }
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    closeCameras();
+
+    IGXFactory::GetInstance().Uninit();
+}
+
+void MainWindow::addLog(const QString &msg)
+{
+    ui->ocrResult->append(
+        QTime::currentTime().toString("HH:mm:ss") +
+        "  " + msg);
+}
+
+void MainWindow::closeCameras()
+{
+    try
+    {
+        if (m_stream1)
+            m_stream1->StopGrab();
+
+        if (m_stream2)
+            m_stream2->StopGrab();
+
+        m_stream1.reset();
+        m_stream2.reset();
+
+        m_device1.reset();
+        m_device2.reset();
+    }
+    catch (...)
+    {
+    }
 }
--- MainWindow.h
+++ MainWindow.h
@@ -1,7 +1,12 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QImage>
+#include <QMutex>
+
+#include "GalaxyIncludes.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,15 +14,114 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+class MainWindow;
+
+//==========================================================
+// Daheng Capture Callback
+//==========================================================
+class CaptureEvent : public ICaptureEventHandler
+{
+public:
+    CaptureEvent(MainWindow* wnd, int index);
+
+    virtual void DoOnImageCaptured(CImageDataPointer& objImageDataPointer,
+                                   void* pUserParam);
+
+private:
+    MainWindow* m_mainWindow;
+    int m_cameraIndex;
+};
+
+//==========================================================
+// Main Window
+//==========================================================
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
+    ~MainWindow();
+
+signals:
+
+    void frameReady(int cameraIndex, const QImage& image);
+
+private slots:
+
+    void onFrameReady(int cameraIndex, const QImage& image);
 
 private:
+
+    //--------------------------------------------------
+    // SDK
+    //--------------------------------------------------
+    bool initializeGalaxy();
+    void detectCameras();
+    bool openCamera(int index);
+    void startGrab(int index);
+    void stopGrab(int index);
+    void closeCameras();
+
+    //--------------------------------------------------
+    // UI
+    //--------------------------------------------------
+    void initializeGraphicsView();
+    void addLog(const QString& text);
+
+    //--------------------------------------------------
+    // Callback Helper
+    //--------------------------------------------------
+    friend class CaptureEvent;
+
+    void processImage(int cameraIndex,
+                      CImageDataPointer& image);
+
+private:
+
     Ui::MainWindow *ui;
+
+    //--------------------------------------------------
+    // Graphics Views
+    //--------------------------------------------------
+    QGraphicsScene* sceneLive = nullptr;
+    QGraphicsScene* sceneCam1 = nullptr;
+    QGraphicsScene* sceneCam2 = nullptr;
+
+    QGraphicsPixmapItem* itemLive = nullptr;
+    QGraphicsPixmapItem* itemCam1 = nullptr;
+    QGraphicsPixmapItem* itemCam2 = nullptr;
+
+    //--------------------------------------------------
+    // Galaxy SDK
+    //--------------------------------------------------
+    bool m_sdkInitialized = false;
+
+    bool m_camera1Connected = false;
+    bool m_camera2Connected = false;
+
+    gxdeviceinfo_vector m_deviceList;
+
+    CGXDevicePointer m_device1;
+    CGXDevicePointer m_device2;
+
+    CGXStreamPointer m_stream1;
+    CGXStreamPointer m_stream2;
+
+    CGXFeatureControlPointer m_feature1;
+    CGXFeatureControlPointer m_feature2;
+
+    CGXFeatureControlPointer m_streamFeature1;
+    CGXFeatureControlPointer m_streamFeature2;
+
+    //--------------------------------------------------
+    // Callback Objects
+    //--------------------------------------------------
+    CaptureEvent* m_capture1 = nullptr;
+    CaptureEvent* m_capture2 = nullptr;
+
+    //--------------------------------------------------
+    // Thread Safety
+    //--------------------------------------------------
+    QMutex m_mutex;
 };
-#endif // MAINWINDOW_H
